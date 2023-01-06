9414f86e5172761376edc7943b34d70027d005cf
Zigijos <zigijos@gmail.com>
2023-01-06 13:22:32 -0800

    commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 
 """Print the ASCII alphabets in lowercase except q and e """
 for letter in range(97, 123):
-	if chr(letter) != 'q' and chr(letter) != 'e':
-		print("{}".format(chr(letter)), end="")
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
