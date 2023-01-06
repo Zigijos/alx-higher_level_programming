7a889d1a49d3fbf0b6e5422bb5ad91315a1daae8
Zigijos <zigijos@gmail.com>
2023-01-06 19:58:37 +0000

    commit

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,29 +9,29 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current = *head;
-	listint_t *newnode = NULL;
+		listint_t *current = *head;
+		listint_t *newnode = NULL;
 
-	newnode = malloc(sizeof(listint_t));
-
-	if (!newnode)
-		return (NULL);
-	newnode->n = number;
-	newnode->next = NULL;
-	if (!*head)
-	{
-		*head = newnode;
+		newnode = malloc(sizeof(listint_t));
+		
+		if (!newnode)
+				return (NULL);
+		newnode->n = number;
+		newnode->next = NULL;
+		if (!*head)
+		{
+				*head = newnode;
+				return (newnode);
+		}
+		if (newnode->n < current->next->n)
+		{
+				newnode->next = *head;
+				*head = newnode
+						return (newnode);
+		}
+		while (current->next != NULL && current->next->n < newnode->n)
+				current = current->next;
+		newnode->next = current->next;
+		current->next = newnode;
 		return (newnode);
-	}
-	if (newnode->n < current->next->n)
-	{
-		newnode->next = *head;
-		*head = newnode
-		return (newnode);
-	}
-	while (current->next != NULL && current->next->n < newnode->n)
-		current = current->next;
-	newnode->next = current->next;
-	current->next = newnode;
-	return (newnode);
 }
