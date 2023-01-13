c3b359d887432d5e7f39224c574d5b50681fe548
Zigijos <zigijos@gmail.com>
2023-01-13 21:40:45 +0000

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
 
-    roman_dict =
-    {
+    roman_dict = {
         "I": 1,
         "V": 5,
         "X": 10,
