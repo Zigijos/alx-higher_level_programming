d8fed6a19881d4e8ea47bef4f0bf43f011d1fed3
Zigijos <zigijos@gmail.com>
2023-01-28 00:24:03 +0000

    commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -11,4 +11,4 @@ def safe_function(fct, *args):
         return (funct_result)
     except ValueError:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-    return (None)
+        return (None)
