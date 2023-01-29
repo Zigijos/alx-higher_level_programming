f35a8921479bb41b3406d65835c5f18d049ee732
Zigijos <zigijos@gmail.com>
2023-01-29 22:20:15 +0000

    commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    def __init__(self, size=0):
+
+        self.__size = size
+
+    def area(self):
+
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+
+        if self.__size > 0:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
