6fd7949dc36637aa90575bb9efb8628327e9b117
Zigijos <zigijos@gmail.com>
2023-01-29 22:43:50 +0000

    commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""This module creates a class named Square"""
 
 
 class Square:
+    """A class named Square
+    Attributes:
+    attr1 (size): size of square"""
 
     def __init__(self, size=0):
+        """Args:
+        size (int): size for __size attribute of class instance"""
 
         if type(size) != int:
             raise TypeError("size must be an integer")
