96a7b68697cbd7e60242810386b653d577198cd4
Zigijos <zigijos@gmail.com>
2023-01-29 18:29:28 +0000

    commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,8 +4,8 @@
 def safe_print_division(a, b):
 
     try:
-        c = a + b
-    except ValueError:
+        c = a / b
+    except (ValueError, TypeError, ZeroDivisionError):
         c = None
     finally:
         print("Inside result: {}".format(c))
