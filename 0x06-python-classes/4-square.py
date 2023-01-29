b91cabf918ee3f71e35b2897f43859cf6b547ae6
Zigijos <zigijos@gmail.com>
2023-01-29 23:27:45 +0000

    commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
+"""This module creates a class named Square"""
 
 
 class Square:
+    """A class named Square
+    Attributes:
+    attr1 (size): size of square"""
 
     def __init__(self, size=0):
+        """Args:
+        size (int): size for __size attribute of class instance"""
 
         self.__size = size
 
     def area(self):
+        """Calculates the area based on size of square
+        Returns:
+        int: The return value. Returns the area"""
 
         return self.__size * self.__size
 
     @property
     def size(self):
+        """Gets the size of the class instance"""
 
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Sets the size of the class instance"""
 
         if type(value) != int:
             raise TypeError("size must be an integer")
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+"""This module creates a class named Square"""
 
 
 class Square:
+    """A class named Square
+    Attributes:
+    attr1 (size): size of square"""
 
     def __init__(self, size=0):
+        """Args:
+        size (int): size for __size attribute of class instance"""
 
         self.__size = size
 
     def area(self):
+        """Calculates the area based on size of square
+        Returns: int: The return value. Returns the area"""
 
         return self.__size * self.__size
 
     @property
     def size(self):
-
+        """Gets the size of the class instance"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Sets the size of the class instance"""
 
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -26,6 +35,7 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """Prints in stdout the square with the # character"""
 
         if self.__size > 0:
             for row in range(self.__size):
